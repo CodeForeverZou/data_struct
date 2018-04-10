673d05df9f88fc3b469ee9c5d107c7dd0f8f3ba6
Coder <1187736527@qq.com>
2018-04-10 10:30:44 +0800

    中序遍历非递归（两种）

--- Tree/Tree/Tree/tree_methord.h
+++ Tree/Tree/Tree/tree_methord.h
@@ -67,6 +67,24 @@ void PreOrder(BTree T) {
 	}
 }
 
+void InOrder(BTree T) {
+	if (T)
+	{
+		InOrder(T->lchild);
+		Visit(T);
+		InOrder(T->rchild);
+	}
+}
+
+void PostOrder(BTree T) {
+	if (T)
+	{
+		PostOrder(T->lchild);
+		PostOrder(T->rchild);
+		Visit(T);
+	}
+}
+
 void N_PreOrder(BTree T) {
 	Stack S;
 	InitStack(S);
@@ -99,4 +117,62 @@ void N_PreOrder(BTree T) {
 			}
 		}
 	}
+	printf("\n");
+}
+
+void N_InOrder(BTree T) {
+	Stack S;
+	InitStack(S);
+	BTNode *p=T;
+	while (p||S.top>0)
+	{
+		if (p)
+		{
+			Push(S, p);
+			p = p->lchild;
+		}
+		else
+		{
+			p=Pop(S);
+			Visit(p);
+			p = p->rchild;
+		}
+	}
+	printf("\n");
+}
+
+void N2_InOrder(BTree T) {
+	Stack S;
+	InitStack(S);
+	BTNode *p = T;
+	Push(S, p);
+	while (S.top>0)
+	{
+		while (p = S.node[S.top - 1]) {		//GerTop(S)
+			p = p->lchild;
+			Push(S, p);
+		}
+		Pop(S);						//¿ÕÖ¸Õë³öÕ»
+		if (S.top ==0) break;		//ÅÐ¶ÏÕ»¿Õ£¬ÓÃ== £¡£¡£¡
+		if (p=Pop(S))
+		{
+			Visit(p);
+			Push(S, p->rchild);
+		}
+	}
+	/*
+	while (p||S.top>0)
+	{
+		while (p) {
+			Push(S, p);
+			p = p->lchild;
+		}
+		if (p = Pop(S)) {
+			Visit(p);
+			p = p->rchild;
+			Push(S, p);
+		}
+	}
+	*/
+	printf("\n");
 }
